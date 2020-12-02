a1928973c09eba4d9bc42d77ad98bb69ccb97e1b
matthew45678 <53129410+matthew45678@users.noreply.github.com>
2020-12-02 14:31:03 +0100

    added products lists and footer

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,18 @@
-document.addEventListener('scroll',()=>{
+let lists = document.querySelectorAll('.productList>ul');
+lists.forEach((element)=>{
+    element.addEventListener('wheel',(e)=>{
+        let list = element.querySelector('ul');
+        let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        if(!(element.scrollLeft+element.clientWidth == element.scrollWidth && delta<0)^(element.scrollLeft == 0 && delta>0))
+        {
+            // console.log('l')
+            element.scrollLeft-=delta*30;
+            e.preventDefault();
+        }
+    })
+})
+
+document.addEventListener('scroll',(event,delta)=>{
     const header = document.querySelector('header');
     header.querySelector('h1').style.transitionDuration="0.1s"
     if(window.scrollY>0){
@@ -10,4 +24,4 @@ document.addEventListener('scroll',()=>{
             header.classList.remove('scrollHeader');
         }
     }
-})
+});
